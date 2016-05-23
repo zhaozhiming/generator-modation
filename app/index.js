f6deaec0bce91743dc2e1509334f2b3b6a905e09
zhaozhiming <kingzzm1982@sina.com>
2016-05-23 10:52:25 +0800

    add dev dependency

--- app/index.js
+++ app/index.js
@@ -5,7 +5,35 @@ module.exports = generators.Base.extend({
     generators.Base.apply(this, arguments);
   },
 
+  prompting: function () {
+    var prompts = [
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Your project name',
+        default: this.appname,
+      },
+      {
+        type: 'input',
+        name: 'projectDesc',
+        message: 'Your project description',
+      },
+    ];
+
+    return this.prompt(prompts).then(function (props) {
+      this.props = props;
+    }.bind(this));
+  },
+
   writing: function() {
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      {
+        projectName: this.props.projectName,
+        projectDesc: this.props.projectDesc,
+      }
+    );
     this.fs.copy(
       this.templatePath('docs/README.md'),
       this.destinationPath('docs/README.md')
@@ -18,5 +46,5 @@ module.exports = generators.Base.extend({
       this.templatePath('src/**/*.*'),
       this.destinationPath('src')
     );
-  }
+  },
 });
